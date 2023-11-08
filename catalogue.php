d9d619d422c7f02df40485457d578759d8855097
laksh2605 <laksh_26@icloud.com>
2023-11-08 12:02:07 +0000

    catalogue

--- catalogue.php
+++ catalogue.php
@@ -20,21 +20,45 @@
                 // Retrieve books from the TblBooks table
                 $query = "SELECT * FROM TblBooks";
                 $result = $conn->query($query);
-
                 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                     echo "<tr>";
                     echo "<td>" . $row['Title'] . "</td>";
                     echo "<td>" . $row['Author'] . "</td>";
                     echo "<td>" . $row['ISBN'] . "</td>";
                     echo "<td>" . $row['In_Library'] . "</td>";
-
                     echo "</tr>";
+                }
+        
+                // Search by Title
+                if (isset($_POST['searchTitle'])) {
+                    $searchTitle = $_POST['searchTitle'];
+        
+                    // Prepare a SQL query to search for books by title
+                    $searchTitleQuery = $conn->prepare("SELECT * FROM TblBooks WHERE Title LIKE :searchTitle");
+                    $searchTitleQuery->bindParam(':searchTitle', '%' . $searchTitle . '%', PDO::PARAM_STR);
+                    $searchTitleQuery->execute();
+        
+                    while ($row = $searchTitleQuery->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<tr>";
+                        echo "<td>" . $row['Title'] . "</td>";
+                        echo "<td>" . $row['Author'] . "</td>";
+                        echo "<td>" . $row['ISBN'] . "</td>";
+                        echo "<td>" . $row['In_Library'] . "</td>";
+                        echo "</tr>";
+                    }
                 }
             } catch (PDOException $e) {
                 echo "Error: " . $e->getMessage();
             }
         ?>
     </table>
+
+    <h2>Search by Title</h2>
+    <form method="POST">
+        <label for="searchTitle">Title: </label>
+        <input type="text" name="searchTitle" id="searchTitle" placeholder="Enter Book Title">
+        <input type="submit" value="Search">
+    </form>
 </body>
 </html>
 
--- catalogue2.php
+++ catalogue2.php
@@ -6,57 +6,60 @@
 <body>
     <h1>Library Catalogue</h1>
 
-    <table>
-        <tr>
-            <th>Book Title: </th>
-            <th>Author: </th>
-            <th>ISBN: </th>
-            <th>In Library: </th>
-        </tr>
+    <h2>Search by Title</h2>
+    <form method="POST">
+        <label for="searchTitle">Title: </label>
+        <input type="text" name="searchTitle" id="searchTitle" placeholder="Enter Book Title">
+        <input type="submit" value="Search">
+    </form>
 
-        <?php
-        // Include the database connection file
-        include 'connection.php';
+    <?php
+    // Include the database connection file
+    include 'connection.php';
 
-        // Display all books
+    if (isset($_POST['searchTitle'])) {
+        $searchTitle = $_POST['searchTitle'];
+
+        $searchTitleWildcard = '%' . $searchTitle . '%';
+
+        $searchTitleQuery = $conn->prepare("SELECT * FROM TblBooks WHERE Title LIKE :searchTitle");
+        $searchTitleQuery->bindParam(':searchTitle', $searchTitleWildcard, PDO::PARAM_STR);
+        $searchTitleQuery->execute();
+
+        echo '<h2>Search Results</h2>';
+        echo '<table>';
+        echo '<tr><th>Book Title:</th><th>Author:</th><th>ISBN:</th><th>In Library:</th></tr>';
+
+        while ($row = $searchTitleQuery->fetch(PDO::FETCH_ASSOC)) {
+            echo '<tr>';
+            echo '<td>' . $row['Title'] . '</td>';
+            echo '<td>' . $row['Author'] . '</td>';
+            echo '<td>' . $row['ISBN'] . '</td>';
+            echo '<td>' . $row['In_Library'] . '</td>';
+            echo '</tr>';
+        }
+
+        echo '</table>';
+    } else {
+        // Display all books by title
         $query = "SELECT * FROM TblBooks";
         $result = $conn->query($query);
 
+        echo '<h2>All Books</h2>';
+        echo '<table>';
+        echo '<tr><th>Book Title:</th><th>Author:</th><th>ISBN:</th><th>In Library:</th></tr>';
+
         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            echo "<tr>";
-            echo "<td>" . $row['Title'] . "</td>";
-            echo "<td>" . $row['Author'] . "</td>";
-            echo "<td>" . $row['ISBN'] . "</td>";
-            echo "<td>" . $row['In_Library'] . "</td>";
-            echo "</tr>";
+            echo '<tr>';
+            echo '<td>' . $row['Title'] . '</td>';
+            echo '<td>' . $row['Author'] . '</td>';
+            echo '<td>' . $row['ISBN'] . '</td>';
+            echo '<td>' . $row['In_Library'] . '</td>';
+            echo '</tr>';
         }
 
-        // Search by Author
-        if (isset($_POST['searchAuthor'])) {
-            $searchAuthor = $_POST['searchAuthor'];
-
-            // Prepare a SQL query to search for books by author
-            $searchAuthorQuery = $conn->prepare("SELECT * FROM TblBooks WHERE Author LIKE :searchAuthor");
-            $searchAuthorQuery->bindParam(':searchAuthor', '%' . $searchAuthor . '%', PDO::PARAM_STR);
-            $searchAuthorQuery->execute();
-
-            while ($row = $searchAuthorQuery->fetch(PDO::FETCH_ASSOC)) {
-                echo "<tr>";
-                echo "<td>" . $row['Title'] . "</td>";
-                echo "<td>" . $row['Author'] . "</td>";
-                echo "<td>" . $row['ISBN'] . "</td>";
-                echo "<td>" . $row['In_Library'] . "</td>";
-                echo "</tr>";
-            }
-        }
-        ?>
-    </table>
-
-    <h2>Search by Author</h2>
-    <form method="POST">
-        <label for="searchAuthor">Author: </label>
-        <input type="text" name="searchAuthor" id="searchAuthor" placeholder="Enter Author Name">
-        <input type="submit" value="Search">
-    </form>
+        echo '</table>';
+    }
+    ?>
 </body>
 </html>
