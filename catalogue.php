7b9674051ad44fa45f4ca65f71434915b5100599
laksh2605 <laksh_26@icloud.com>
2023-11-10 22:46:39 +0000

    catalogue

--- catalogue.php
+++ catalogue.php
@@ -6,11 +6,15 @@
 <body>
     <h1>Library Catalogue</h1>
 
-    <!-- Search by Title Form -->
-    <h2>Search by Title</h2>
+    <!-- Search Form -->
+    <h2>Search Books</h2>
     <form method="POST">
         <label for="searchTitle">Title: </label>
         <input type="text" name="searchTitle" id="searchTitle" placeholder="Enter Book Title">
+
+        <label for="searchAuthor">Author: </label>
+        <input type="text" name="searchAuthor" id="searchAuthor" placeholder="Enter Author Name">
+
         <input type="submit" value="Search">
     </form>
 
@@ -19,26 +23,37 @@
     include 'connection.php';
 
     try {
-        // Display the search results or all books based on the condition
-        if (isset($_POST['searchTitle'])) {
+        // Build the SQL query based on user input
+        $query = "SELECT * FROM TblBooks WHERE 1";
+
+        // Check if title is provided for search
+        if (isset($_POST['searchTitle']) && !empty($_POST['searchTitle'])) {
             $searchTitle = $_POST['searchTitle'];
-
-            // Prepare a SQL query to search for books by title
-            $searchTitleValue = '%' . $searchTitle . '%';
-            $searchTitleQuery = $conn->prepare("SELECT * FROM TblBooks WHERE Title LIKE :searchTitle");
-            $searchTitleQuery->bindParam(':searchTitle', $searchTitleValue, PDO::PARAM_STR);
-            $searchTitleQuery->execute();
-
-            echo '<h2>Search Results</h2>';
-        } else {
-            // Retrieve all books from the TblBooks table
-            $query = "SELECT * FROM TblBooks";
-            $searchTitleQuery = $conn->query($query);
-
-            echo '<h2>All Books</h2>';
+            $query .= " AND Title LIKE :searchTitle";
         }
 
-        // Display the table header
+        // Check if author is provided for search
+        if (isset($_POST['searchAuthor']) && !empty($_POST['searchAuthor'])) {
+            $searchAuthor = $_POST['searchAuthor'];
+            $query .= " AND Author LIKE :searchAuthor";
+        }
+
+        // Prepare and execute the SQL query
+        $searchQuery = $conn->prepare($query);
+
+        if (isset($searchTitle)) {
+            $searchTitleValue = '%' . $searchTitle . '%';
+            $searchQuery->bindParam(':searchTitle', $searchTitleValue, PDO::PARAM_STR);
+        }
+
+        if (isset($searchAuthor)) {
+            $searchAuthorValue = '%' . $searchAuthor . '%';
+            $searchQuery->bindParam(':searchAuthor', $searchAuthorValue, PDO::PARAM_STR);
+        }
+
+        $searchQuery->execute();
+
+        echo '<h2>Search Results</h2>';
         echo '<table>';
         echo '<tr>
                 <th>Book Title: </th>
@@ -47,8 +62,7 @@
                 <th>In Library: </th>
               </tr>';
 
-        // Display the books in the table
-        while ($row = $searchTitleQuery->fetch(PDO::FETCH_ASSOC)) {
+        while ($row = $searchQuery->fetch(PDO::FETCH_ASSOC)) {
             echo '<tr>';
             echo '<td>' . $row['Title'] . '</td>';
             echo '<td>' . $row['Author'] . '</td>';
