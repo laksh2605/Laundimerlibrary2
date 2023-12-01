a68c577004ba93e92d437d12cb92e828943a8b8e
laksh2605 <laksh_26@icloud.com>
2023-12-01 15:45:39 +0000

    loanbooks done

--- loanbookssql.php
+++ loanbookssql.php
@@ -1,9 +1,9 @@
 <?php
-include_once("connection.php"); // Include your connection settings
+include_once("connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Insert a new record into tblloans
     $sql = "INSERT INTO tblloans (UserID, ISBN, Date_Borrowed, Date_Returned) VALUES (:UserID, :ISBN, :Date_Borrowed, :Date_Returned)";
-
     $stmt = $conn->prepare($sql);
 
     $stmt->bindParam(':UserID', $_POST["UserID"]);
@@ -12,11 +12,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bindParam(':Date_Returned', $_POST["Date_Returned"]);
 
     if ($stmt->execute()) {
-        echo "New record created successfully";
+        // Update In_Library field in TblBooks to 'N'
+        $updateSQL = "UPDATE TblBooks SET In_Library = 'N' WHERE ISBN = :ISBN";
+        $updateStmt = $conn->prepare($updateSQL);
+        $updateStmt->bindParam(':ISBN', $_POST["ISBN"]);
+
+        if ($updateStmt->execute()) {
+            echo "New record created successfully.";
+        } else {
+            echo "Error updating book status: " . $updateStmt->errorInfo()[2];
+        }
+
+        $updateStmt->closeCursor();
     } else {
-        echo "Error: " . $stmt->errorInfo()[2]; // Display detailed error information
+        echo "Error: " . $stmt->errorInfo()[2];
     }
 
     $stmt->closeCursor();
 }
 ?>
+
+
